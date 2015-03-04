c5393ef5abb2e5055d52b09835341e2e5907a97f
Nhu Finney & John Franti <student@epicodus.com>
2015-03-04 11:06:37 -0800

    Add delete button.

--- app/app.php
+++ app/app.php
@@ -10,13 +10,25 @@
     $app = new Silex\Application();
 
     $app->get("/", function(){
+
         $output="";
 
-        foreach (Task::getAll() as $task) {
-            $output = $output . "<p>" . $task->getDescription() . "</p>";
+        $all_tasks = Task::getAll();
+
+        if (!empty($all_tasks)) {
+            $output .= "
+            <h1>To Do List</h1>
+            <p>Here are all your tasks:</p>
+            <ul>";
+
+            foreach ($all_tasks as $task) {
+                $output .= "<p>" . $task->getDescription() . "</p>";
+            }
+
+            $output .= "</ul>";
         }
 
-        $output = $output . "<ul>
+        $output .= "<ul>
             <form action='/tasks' method='post'>
                 <label for='description'>Task Description</label>
                 <input id='description' name='description' type='text'>
@@ -24,6 +36,12 @@
                 <button type='submit'>Add task</button>
             </form>
         ";
+
+        $output .= "
+            <form action='/delete_tasks' method='post'>
+                <button type='submit'>delete</button>
+                </form>
+            ";
 
         return $output;
     });
@@ -37,6 +55,16 @@
             <p><a href='/'>View your list of things to do.</a></p>
         ";
     });
-    
+
+    $app->post("/delete_tasks", function() {
+
+        Task::deleteAll();
+
+        return "
+            <h1>List Cleared</h1>
+            <p><a href='/'>Home</a></p>
+        ";
+    });
+
     return $app;
 ?>
