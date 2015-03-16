8c0112df8fd36e9bf04abbe16c10fa956915d859
John Franti & Patrick Fiorentino <student@epicodus.com>
2015-03-16 15:28:02 -0700

    Fix webpage routing.

--- app/app.php
+++ app/app.php
@@ -1,31 +1,50 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Task.php";
-
-    $DB = new PDO('pgsql:host=localhost;dbname=to_do');
+    require_once __DIR__."/../src/Category.php";
 
     $app = new Silex\Application();
+
+    $app['debug']=true;
+
+    $DB = new PDO('pgsql:host=localhost;dbname=to_do');
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
         'twig.path' => __DIR__.'/../views'
     ));
 
     $app->get("/", function() use ($app) {
+        return $app['twig']->render('index.html.twig');
+    });
 
-        return $app['twig']->render('tasks.php', array('tasks' => Task::getALL()));
+    $app->get("/tasks", function() use ($app) {
+        return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
+    });
+
+    $app->get("/categories", function() use ($app) {
+        return $app['twig']->render('categories.html.twig', array('categories' => Category::getAll()));
+    });
+
+    $app->post("/categories", function() use ($app) {
+        $category = new Category($_POST['name']);
+        $category->save();
+        return $app['twig']->render('categories.html.twig', array('categories' => Category::getAll()));
+    });
+
+    $app->post("/delete_categories", function() use ($app) {
+        Category::deleteAll();
+        return $app['twig']->render('index.html.twig');
     });
 
     $app->post("/tasks", function() use ($app) {
         $task = new Task($_POST['description']);
         $task->save();
-
-        return $app['twig']->render('create_task.php', array('newtask' => $task));
-
+        return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
     });
 
     $app->post("/delete_tasks", function() use ($app) {
         Task::deleteAll();
-        return $app['twig']->render('delete_tasks.php');
+        return $app['twig']->render('index.html.twig');
     });
 
     return $app;
